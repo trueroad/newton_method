59529216448f237eb31a10dd5759c42d8633c0dd
Masamichi Hosoda <trueroad@trueroad.jp>
2018-08-24 00:56:05 +0900

    Bump up license etc.

--- sample-weighted.cc
+++ sample-weighted.cc
@@ -1,25 +1,30 @@
 //
-// Newton's method library to solve simultaneous equations 2017-06-24.16
-// https://github.com/trueroad/newton_method/
+// Newton's method library to solve simultaneous equations 2018-08-23.15
+// https://github.com/trueroad/newton_method
 //
-// Sample: Position calculation like GPS/GNSS receiver
-//         (overdetermined system, weighted)
+// sample-weighted.cc:
+//   Sample for position calculation like GPS/GNSS receiver
+//   (overdetermined system, weighted)
 //
-// Copyright (C) 2017 Masamichi Hosoda. All rights reserved.
+// Copyright (C) 2017, 2018 Masamichi Hosoda.
+// All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
 //
-// THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+// ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
 // FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 // DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
 // OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
