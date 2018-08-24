91653c380168e95711921f901d50fdb2711bd3a5
Masamichi Hosoda <trueroad@trueroad.jp>
2018-08-24 22:42:09 +0900

    Build only the algorithms needed in samples

---
+++ newton_method/solve.mk
@@ -0,0 +1,8 @@
+# This `solve.mk` builds only the algorithms needed in samples.
+# If all algorithms are required, remove this `solve.mk` then invoke `make`.
+# `make-solve-ml.pl` will regenerate `solve.mk` for all algorithms.
+
+OBJS_SOLVE = \
+	solve-through_pass-ColPivHouseholderQR.o \
+	solve-weighted-ColPivHouseholderQR.o \
+
